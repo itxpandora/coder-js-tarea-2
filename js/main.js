6c9d8c9c48f6af4a46053d14a3d82676696c6ea4
Pandora <delasierramanu@gmail.com>
2026-07-27 22:06:30 -0300

    2nd try

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-let edad = parseInt(prompt("Ingresa tu edad:"));
-let compania = prompt("Estas acompañado? (si/no)").toLowerCase();
+let pasaron = 0;
+let rebotados = 0;
 
-if (edad >= 18) {     
-    console.log("Podes pasar.");
-} else if (edad >= 12 && compania === "si") {     
-    console.log("Podes pasar, pero con tu acompañante.");
-} else {
-    console.log("No podes pasar.");
-}
+do {
+    let edad = parseInt(prompt("Ingresa tu edad:"));
+    let compania = prompt("Estas acompañado? (si/no)").toLowerCase();
+
+    if (edad >= 18) {     
+        console.log("Podes pasar.");
+        pasaron++;
+    } else if (edad >= 12 && compania === "si") {     
+        console.log("Podes pasar, pero con tu acompañante.");
+        pasaron++;
+    } else {
+        console.log("No podes pasar.");
+        rebotados++;
+    }
+
+} while (confirm("¿Querés evaluar a otra persona?"));
+
+console.log("--- RESUMEN DE HOY ---");
+console.log("Total evaluados: ${pasaron + rebotados}");
+console.log("Ingresaron: ${pasaron}");
+console.log("Rebotados: ${rebotados}");
